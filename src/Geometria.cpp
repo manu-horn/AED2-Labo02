5c61b6f39a1175f21bf9e9062f5cd62c405709d1
manu-horn <manuhorn1910@gmail.com>
2022-09-02 21:10:51 -0300

    Ejercicios 1 - 13 terminados Faltan: 14 y 15

--- src/Geometria.cpp
+++ src/Geometria.cpp
@@ -19,20 +19,54 @@ class Rectangulo {
 
 };
 
-Rectangulo::Rectangulo(uint alto, uint ancho) /* Completar */ {};
+Rectangulo::Rectangulo(uint alto, uint ancho) : _alto(alto), _ancho(ancho) {};
 
 uint Rectangulo::alto() {
     // Completar
-    return 0;
+    return _alto;
 }
 
 // Completar definición: ancho
+uint Rectangulo::ancho() {
+    // Completar
+    return _ancho;
+}
 
 // Completar definición: area
-
+float Rectangulo::area() {
+    // Completar
+    return (_alto * _ancho);
+}
 // Ejercicio 2
 
 // Clase Elipse
+
+class Elipse {
+    public:
+    Elipse(uint a, uint b);
+    uint r_a();
+    uint r_b();
+    float area();
+
+    private:
+    float PI = 3.14;
+    int r_a_;
+    int r_b_;
+};
+
+Elipse::Elipse(uint a, uint b) : r_a_(a), r_b_(b) {}
+
+uint Elipse::r_b() {
+    return r_b_;
+}
+
+uint Elipse::r_a() {
+    return r_a_;
+}
+
+float Elipse::area() {
+    return (Elipse::PI * r_a_ * r_b_);
+}
 
 // Ejercicio 3
 
@@ -50,27 +84,67 @@ Cuadrado::Cuadrado(uint lado): _r(lado, lado) {}
 
 uint Cuadrado::lado() {
     // Completar
-    return 0;
+    return _r.alto();
 }
 
 float Cuadrado::area() {
     // Completar
-    return 0;
+    return _r.area();
 }
 
 // Ejercicio 4
 
 // Clase Circulo
 
+class Circulo{
+    public:
+    Circulo(uint radio);
+    uint radio();
+    float area();
+    ;
+    private: // uso clase Elipse
+    Elipse e_;
+    ;
+};
 
-// Ejercicio 5
+Circulo::Circulo(uint radio) :  e_(radio, radio) {};
+
+uint Circulo::radio() {
+    return e_.r_a();
+}
+
+float Circulo::area() {
+    return e_.area();
+};
+
+
+
+// Ejercicio 5: Imprimir en pantalla
 
 ostream& operator<<(ostream& os, Rectangulo r) {
-    os << "Rect(" << r.ancho() << ", " << r.alto() << ")";
+    os << "Rect(" << r.alto() << ", " << r.ancho() << ")";
     return os;
 }
 
 // ostream Elipse
 
+ostream& operator<<(ostream& os, Elipse e) {
+    os << "Elipse(" << e.r_a() << ", " << e.r_b() << ")";
+    return os;
+}
+
 // Ejercicio 6
 
+// circulo
+
+ostream& operator<<(ostream& os, Circulo c) {
+    os << "Circ(" << c.radio() << ")";
+    return os;
+}
+
+// cuadrado
+
+ostream& operator<<(ostream& os, Cuadrado c) {
+    os << "Cuad(" << c.lado() << ")";
+    return os;
+}
--- src/Recordatorios.cpp
+++ src/Recordatorios.cpp
@@ -21,34 +21,162 @@ uint dias_en_mes(uint mes) {
 class Fecha {
   public:
     // Completar declaraciones funciones
+    Fecha(int mes, int dia);
+    int mes();
+    int dia();
+    //ej 10
+    void incrementar_dia();
     #if EJ >= 9 // Para ejercicio 9
     bool operator==(Fecha o);
     #endif
 
   private:
     //Completar miembros internos
+    int mes_;
+    int dia_;
 };
 
+int Fecha::mes() {
+    return mes_;
+}
+
+int Fecha::dia() {
+    return dia_;
+}
+
+Fecha::Fecha(int mes, int dia) : mes_(mes), dia_(dia) {} ;
+
+//8 ostream fecha
+
+ostream& operator<<(ostream& os, Fecha f) {
+    os << f.dia() << "/" << f.mes();
+    return os;
+}
 
 #if EJ >= 9
 bool Fecha::operator==(Fecha o) {
-    bool igual_dia = this->dia() == o.dia();
+    bool igual_dia = (this->dia() == o.dia()
+            && this-> mes() == o.mes());
     // Completar iguadad (ej 9)
     return igual_dia;
 }
+
+// 10
+void Fecha::incrementar_dia() {
+    if (dia_ < dias_en_mes(mes_)){
+        dia_++;
+    } else{ // dia_ == dias_en_mes(mes_)
+        dia_ = 1;
+        if (mes_ == 12){
+            mes_ = 1;
+        } else{
+            mes_++;
+        }
+    }
+}
+
 #endif
+
 
 // Ejercicio 11, 12
 
 // Clase Horario
 
+class Horario {
+    public:
+    Horario(uint hora, uint min);
+    uint hora();
+    uint min();
+    bool operator==(Horario o);
+
+    //12
+    bool operator<(Horario o);
+
+    private:
+    uint hora_;
+    uint min_;
+};
+
+Horario::Horario(uint hora, uint min) : hora_(hora), min_(min){};
+
+uint Horario::hora() {
+    return hora_;
+};
+
+uint Horario::min() {
+    return min_;
+};
+
+//ostream horario << y ==
+
+ostream& operator<<(ostream& os, Horario h) {
+    os << h.hora() << ":" << h.min();
+    return os;
+};
+
+bool Horario::operator==(Horario o) {
+    bool igual_Hora = (this->hora() == o.hora()
+                      && this-> min() == o.min());
+    return igual_Hora;
+}
+
+bool Horario::operator<(Horario o) {
+    bool esMenor;
+    if (this->hora() < o.hora()){
+        esMenor = true;
+    } else{ // >=
+        if(this->hora() > o.hora()){
+            esMenor = false;
+        } else {
+            esMenor = this->min() < o.min();
+        }
+    }
+    return esMenor;
+};
 
 // Ejercicio 13
 
 // Clase Recordatorio
 
+class Recordatorio{
+    public:
+    Recordatorio(Fecha f, Horario h, string mensaje);
+    Fecha f();
+    Horario h();
+    string mensaje();
+
+    private:
+    Fecha f_;
+    Horario h_;
+    string mensaje_;
+    ;
+};
+
+Recordatorio::Recordatorio(Fecha f, Horario h, string mensaje) :
+f_(f.mes(), f.dia()), h_(h.hora(), h.min()), mensaje_(mensaje){}
+
+Fecha Recordatorio::f() {
+    return f_;
+}
+
+Horario Recordatorio::h() {
+    return h_;
+}
+
+string Recordatorio::mensaje() {
+    return mensaje_;
+}
+
+// ostream Recordatorio
+
+ostream& operator<<(ostream& os, Recordatorio r) {
+    os << r.mensaje() << " @ " << r.f() << " " << r.h();
+    return os;
+};
 
 // Ejercicio 14
+
+class Agenda{};
 
 // Clase Agenda
 
